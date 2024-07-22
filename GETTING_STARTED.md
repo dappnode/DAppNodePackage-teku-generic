3723ef0501969ffe593371b16c0cea5fc847d3bb
dappnodedev <diego@dappnode.io>
2024-07-22 13:31:53 +0200

    Use latest staker pkg scripts

--- GETTING_STARTED.md
+++ GETTING_STARTED.md
@@ -1,5 +1,3 @@
-## Welcome to the Holesky Consensus Client Teku
+## Welcome to the Consensus Client Teku
 
-- Your keystores should now be managed in the Web3Signer's [New Staking Brain](http://brain.web3signer-holesky.dappnode/)
-- Don't have the Web3Signer installed yet? It can be manually installed [here](http://my.dappnode/installer/dnp/web3signer-holesky.dnp.dappnode.eth)
-- All Staking Management has a new home in the Comprehensive [StakersUI](http://my.dappnode/stakers/holesky)
+- All Staking Management has a new home in the Comprehensive [StakersUI](http://my.dappnode/stakers/ethereum)
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,46 +1,41 @@
 #!/bin/sh
 
 SUPPORTED_NETWORKS="gnosis holesky mainnet lukso"
-MEVBOOST_FLAG_1="--validators-builder-registration-default-enabled=true"
-MEVBOOST_FLAG_2="--validators-proposer-blinded-blocks-enabled=true"
+# MEVBOOST: https://docs.teku.consensys.net/en/latest/HowTo/Builder-Network/
+MEVBOOST_FLAG_KEYS="--validators-builder-registration-default-enabled=true --validators-proposer-blinded-blocks-enabled=true"
 SKIP_MEVBOOST_URL="true"
 CLIENT="teku"
-VALIDATOR_PORT=3500
 
 # shellcheck disable=SC1091
 . /etc/profile
 
-run_validator() {
+VALID_GRAFFITI=$(get_valid_graffiti "${GRAFFITI}")
+VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
+SIGNER_API_URL=$(get_signer_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}")
+BEACON_API_URL=$(get_beacon_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}")
+MEVBOOST_FLAGS=$(get_mevboost_flag "${MEVBOOST_FLAG_KEYS}" "${SKIP_MEVBOOST_URL}")
 
-  echo "[INFO - entrypoint] Starting validator client"
+echo "[INFO - entrypoint] Starting validator client"
 
-  # Teku must start with the current env due to JAVA_HOME var
-  # shellcheck disable=SC2086
-  exec /opt/teku/bin/teku \
-    --log-destination=CONSOLE \
-    validator-client \
-    --network="${NETWORK}" \
-    --data-base-path="${DATA_DIR}" \
-    --beacon-node-api-endpoint="${BEACON_API_URL}" \
-    --validators-external-signer-url="${WEB3SIGNER_API_URL}" \
-    --metrics-enabled=true \
-    --metrics-interface 0.0.0.0 \
-    --metrics-port 8008 \
-    --metrics-host-allowlist=* \
-    --validator-api-enabled=true \
-    --validator-api-interface=0.0.0.0 \
-    --validator-api-port="${VALIDATOR_PORT}" \
-    --validator-api-host-allowlist=* \
-    --validators-graffiti="${GRAFFITI}" \
-    --validator-api-keystore-file="${TLS_CERT_FILE_PATH}" \
-    --validator-api-keystore-password-file="${TLS_CERT_PASS_PATH}" \
-    --validators-proposer-default-fee-recipient="${FEE_RECIPIENT}" \
-    --logging="${LOG_LEVEL}" ${EXTRA_OPTS}
-}
-
-format_graffiti
-set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}"
-# MEVBOOST: https://docs.teku.consensys.net/en/latest/HowTo/Builder-Network/
-set_mevboost_flag "${MEVBOOST_FLAG_1}" "${SKIP_MEVBOOST_URL}"
-set_mevboost_flag "${MEVBOOST_FLAG_2}" "${SKIP_MEVBOOST_URL}"
-run_validator
+# Teku must start with the current env due to JAVA_HOME var
+# shellcheck disable=SC2086
+exec /opt/teku/bin/teku \
+  --log-destination=CONSOLE \
+  validator-client \
+  --network="${NETWORK}" \
+  --data-base-path="${DATA_DIR}" \
+  --beacon-node-api-endpoint="${BEACON_API_URL}" \
+  --validators-external-signer-url="${SIGNER_API_URL}" \
+  --metrics-enabled=true \
+  --metrics-interface 0.0.0.0 \
+  --metrics-port 8008 \
+  --metrics-host-allowlist=* \
+  --validator-api-enabled=true \
+  --validator-api-interface=0.0.0.0 \
+  --validator-api-port="${VALIDATOR_PORT}" \
+  --validator-api-host-allowlist=* \
+  --validators-graffiti="${VALID_GRAFFITI}" \
+  --validator-api-keystore-file="${TLS_CERT_FILE_PATH}" \
+  --validator-api-keystore-password-file="${TLS_CERT_PASS_PATH}" \
+  --validators-proposer-default-fee-recipient="${VALID_FEE_RECIPIENT}" \
+  --logging="${LOG_LEVEL}" ${MEVBOOST_FLAGS} ${EXTRA_OPTS}
