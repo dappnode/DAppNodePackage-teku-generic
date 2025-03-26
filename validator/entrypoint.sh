5847af51b4ae815d75ef1827dfa6da9d216c7cb0
Marc <marcfont12@gmail.com>
2025-03-26 15:57:46 +0100

    add hoodi

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SUPPORTED_NETWORKS="gnosis holesky mainnet lukso"
+SUPPORTED_NETWORKS="gnosis holesky hoodi mainnet lukso"
 # MEVBOOST: https://docs.teku.consensys.net/en/latest/HowTo/Builder-Network/
 MEVBOOST_FLAG_KEYS="--validators-builder-registration-default-enabled=true --validators-proposer-blinded-blocks-enabled=true"
 SKIP_MEVBOOST_URL="true"
