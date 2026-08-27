313522bfde5ae54aa0fc3276f909d88e0b1d1b86
hcastc00 <hcastc00@estudiantes.unileon.es>
2026-08-27 15:04:48 +0200

    fix: update LUKSO version and bootnodes

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -3,12 +3,19 @@
 CHECKPOINT_SYNC_KEY="--checkpoint-sync-url"
 MEVBOOST_FLAG_KEYS="--builder-endpoint"
 
+# https://github.com/lukso-network/network-configs/blob/main/mainnet/teku/teku.yaml
+LUKSO_BOOTNODES="enr:-MK4QHcS3JeTtVjOuJyVXvO1E6XJWqiwmhLfodel6vARPI8ve_2q9vVn8LpIL964qBId7zGpSVKw6oOPAaRm2H7ywYiGAYmHDeBbh2F0dG5ldHOIAAAAAAAAAACEZXRoMpA2ulfbQgAABP__________gmlkgnY0gmlwhCIgwNOJc2VjcDI1NmsxoQNGVC8JPcsqsZPoohLP1ujAYpBfS0dBwiz4LeoUQ-k5OohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A,enr:-Ly4QF7f-m7CiC1EwxmEa3VosxSUAeBaegcOek-J2DgqkB9EHyo5UOCZKAKuGN3pt3S28HVZy1PS6CGILRa8h25pZCsFh2F0dG5ldHOI__________-EZXRoMpCq3eQ4QgAABv__________gmlkgnY0gmlwhLJobp6Jc2VjcDI1NmsxoQJ88Tw_HsGhSdHp8AflKAJnt7VyHNDakc57AT6jdaBihIhzeW5jbmV0cw-DdGNwgiMog3VkcIIjKA,enr:-Jq4QABOsAAluqXVlLuAAWcMtYF__YTHcAWCChtvzXYa2GMuB5bEFaRnsoCuXUHTx4hxABFwwG29eQd3vH4GCZQbgdEBhGV0aDKQ3FGxEUIAAASkHwAAAAAAAIJpZIJ2NIJpcIQ5gLgeiXNlY3AyNTZrMaECWsDTDT1LTmpENdKPc6IbbViy5D1mahB4AJnPOCafYjKDdWRwgiMy"
+
 # shellcheck disable=SC1091 # Path is relative to the Dockerfile
 . /etc/profile
 
 ENGINE_URL="http://execution.${NETWORK}.staker.dappnode:8551"
 VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT_ADDRESS}")
 MEVBOOST_FLAG=$(get_mevboost_flag "${NETWORK}" "${MEVBOOST_FLAG_KEYS}")
+
+if [ "${NETWORK}" = "lukso" ]; then
+    LUKSO_BOOTNODES_FLAG="--p2p-discovery-bootnodes=$LUKSO_BOOTNODES"
+fi
 
 if [ -n "${CHECKPOINT_SYNC_URL}" ]; then
     TEKU_FORMAT_CHECKPOINT_URL="$(echo "${CHECKPOINT_SYNC_URL}" | sed 's:/*$::')"
@@ -35,7 +42,7 @@ FLAGS="--network=$NETWORK \
     --metrics-port=8008 \
     --metrics-host-allowlist=* \
     --log-destination=CONSOLE \
-    --validators-proposer-default-fee-recipient=$VALID_FEE_RECIPIENT $CHECKPOINT_SYNC_FLAG $MEVBOOST_FLAG $EXTRA_OPTS"
+    --validators-proposer-default-fee-recipient=$VALID_FEE_RECIPIENT $LUKSO_BOOTNODES_FLAG $CHECKPOINT_SYNC_FLAG $MEVBOOST_FLAG $EXTRA_OPTS"
 
 echo "[INFO - entrypoint] Starting beacon with flags: $FLAGS"
 
